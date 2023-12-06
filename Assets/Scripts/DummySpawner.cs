479fba2c340eb26b32a66e524a04baf7e9abe812
ArturAbdulkadirov <KS29092003@yandex.com>
2023-12-06 20:03:13 +0300

    Добавлена версия для AR. Билды для AR и PC

--- Assets/Scripts/DummySpawner.cs
+++ Assets/Scripts/DummySpawner.cs
@@ -22,4 +22,15 @@ public class DummySpawner : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ,maxZ));
         Instantiate(dummy, spawnPos, Random.rotation);
     }
+
+#if UNITY_ANDROID
+    private void Update()
+    {
+        maxX = transform.position.x + transform.localScale.x / 2;
+        minX = transform.position.x - transform.localScale.x / 2;
+        
+        maxZ = transform.position.z + transform.localScale.z / 2;
+        minZ = transform.position.z - transform.localScale.z / 2;
+    }
+#endif
 }
